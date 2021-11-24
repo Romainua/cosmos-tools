ae261ef45a7da473b7ef14d7a23ef20e8cce5af6
Your Name <you@example.com>
2021-11-24 20:23:55 +0000

    update

--- tx-spam/run.sh
+++ tx-spam/run.sh
@@ -1,41 +1,44 @@
 #!/bin/bash
-#firstly path
-PS3='Please enter your choice (input your option number and press enter): '
-options=("Install" "Update" "Disable" "Quit")
-select opt in "${options[@]}"
+
+#firstly path                
+function createVarible () {
+        go run main.go
+        cat varible.sh >> ~/.profile
+        source ~/.profile
+        tail -n6  ~/.profile
+        echo -e "\nCheck your variables above☝️"
+        read -n 1 -s -r -p "Press any button to continue";
+}
+#secondly path
+function txSend () {
+	source ~/.profile
+        echo -e "\nLet the spaaaaam start!"
+        sleep 5
+        while :
+do
+        umeed tx bank send $WALLET $RECEIVER $AMOUNT --from $WALLET --fees $FEE --chain-id $CHAIN_ID -y --keyring-backend test --note "$MEMO"
+        echo -e "Still spamming😈"
+        sleep 2
+done;
+}
+PS3='Choose your setting: '
+data=("I'm running for the first time and want to set up variables" "I've already run script before and I don't want to set up variables" "Check varible" )
+select opt in "${data[@]}"
 do
     case $opt in
-        "Install")
-echo "Choose your setting:"
-		echo "I'm running for the first time and want to set up variables"
-		echo "I've already run script before and I don't want to set up variables"
-		read choice 
-		echo $opt
-go run main.go
- 
-cat varible.sh >> ~/.profile
+        "I'm running for the first time and want to set up variables")
+        createVarible
+        txSend
+        ;;
+        "I've already run script before and I don't want to set up variables")
+        txSend
+ 	;;
+	"Check varible")
+		tail -n6  ~/.profile
+		break
+	;;
+*)
+esac
+echo
+done;
 
-source ~/.profile
-
-rm varible.sh
-
-tail -n6  ~/.profile
-
-echo -e "\nCheck your variables above☝️"
-
-read -n 1 -s -r -p "Press any button to continue"
-
-echo -e "1\nLet the spaaaaam start!"
-
-sleep 5
-
-while :
-
-do
-
-        umeed tx bank send $WALLET $RECEIVER $AMOUNT --from $WALLET --fees $FEE --chain-id $CHAIN_ID -y --keyring-backend test --note "$MEMO"
-
-	echo -e "Still spamming😈"
-sleep 2
-
-done
--- tx-spam/varible.sh
+++
@@ -1,6 +0,0 @@
-export WALLET=s
-export RECEIVER=sasd
-export AMOUNT=asd
-export FEE=das
-export CHAIN_ID=dsa
-export MEMO=ds
