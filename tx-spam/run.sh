82685993d9c480de4cd0eadb7cb44eb763da632f
Roman <44613085+Romainua@users.noreply.github.com>
2021-11-24 22:24:52 +0200

    Update run.sh

--- tx-spam/run.sh
+++ tx-spam/run.sh
@@ -5,6 +5,7 @@ function createVarible () {
         go run main.go
         cat varible.sh >> ~/.profile
         source ~/.profile
+	rm varible.sh
         tail -n6  ~/.profile
         echo -e "\nCheck your variables above☝️"
         read -n 1 -s -r -p "Press any button to continue";
